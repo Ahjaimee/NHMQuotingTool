456b803179a65071581769f4ea4fa71eef913f3b
Ahjaimee <jamieacbaker@hotmail.co.uk>
2025-06-07 02:24:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,87 +5,28 @@ const data = {
         labour_hours: 0.5,
         material_cost: 85.0,
         part_number: "OXBATT180"
-      },
-      "Handset Replacement": {
-        labour_hours: 0.75,
-        material_cost: 65.0,
-        part_number: "OXHAND180"
       }
     }
   }
 };
 
-let assetChoices, makeChoices, repairChoices;
+let assetChoices;
 
 document.addEventListener("DOMContentLoaded", () => {
-  populateAssets();
+  const assetSelect = document.getElementById("assetSelect");
 
-  document.getElementById("assetSelect").addEventListener("change", () => {
-    populateMakes();
-    document.getElementById("makeSection").style.display = "block";
+  // Add options to select
+  Object.keys(data).forEach(asset => {
+    const option = document.createElement("option");
+    option.value = asset;
+    option.textContent = asset;
+    assetSelect.appendChild(option);
   });
 
-  document.getElementById("makeSelect").addEventListener("change", () => {
-    populateRepairs();
-    document.getElementById("repairSection").style.display = "block";
-  });
-
-  document.getElementById("repairSelect").addEventListener("change", () => {
-    document.getElementById("optionsSection").style.display = "block";
-  });
+  // Initialize Choices.js
+  assetChoices = new Choices(assetSelect, { searchEnabled: true });
 });
 
-function populateAssets() {
-  const el = document.getElementById("assetSelect");
-  el.innerHTML = "";
-
-  Object.keys(data).forEach(asset => {
-    const opt = document.createElement("option");
-    opt.value = asset;
-    opt.textContent = asset;
-    el.appendChild(opt);
-  });
-
-  if (assetChoices) assetChoices.destroy();
-  assetChoices = new Choices(el, { searchEnabled: true });
-}
-
-function populateMakes() {
-  const asset = document.getElementById("assetSelect").value;
-  const el = document.getElementById("makeSelect");
-  el.innerHTML = "";
-
-  if (data[asset]) {
-    Object.keys(data[asset]).forEach(make => {
-      const opt = document.createElement("option");
-      opt.value = make;
-      opt.textContent = make;
-      el.appendChild(opt);
-    });
-  }
-
-  if (makeChoices) makeChoices.destroy();
-  makeChoices = new Choices(el, { searchEnabled: true });
-}
-
-function populateRepairs() {
-  const asset = document.getElementById("assetSelect").value;
-  const make = document.getElementById("makeSelect").value;
-  const el = document.getElementById("repairSelect");
-  el.innerHTML = "";
-
-  if (data[asset] && data[asset][make]) {
-    Object.keys(data[asset][make]).forEach(repair => {
-      const opt = document.createElement("option");
-      opt.value = repair;
-      opt.textContent = repair;
-      el.appendChild(opt);
-    });
-  }
-
-  if (repairChoices) repairChoices.destroy();
-  repairChoices = new Choices(el, { searchEnabled: true });
-}
 
 
 
